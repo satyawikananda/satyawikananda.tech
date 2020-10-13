6bc23be12a22c0b29ac21ab8b49bfc8ad95154c2
Satya Wikananda <satyawikananda456@gmail.com>
2020-10-13 16:17:32 +0800

    chore: Update accomplishments page

--- components/card/CardProject.vue
+++ components/card/CardProject.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="h-full card-skills shadow-md rounded-lg overflow-hidden relative p-8 flex flex-wrap flex-col items-center justify-center">
     <img :src="image" :alt="title" class="lg:h-48 md:h-36 w-full object-cover rounded-md mb-4">
-    <p class="text-lg md:text-lg text-center font-medium mb-2" :style="{ color: '#ff8906' }">
+    <p class="text-lg md:text-lg text-center font-bold mb-2" :style="{ color: '#ff8906' }">
       {{ title }}
     </p>
     <p class="text-base text-center font-light mb-4">
       {{ description }}
     </p>
-    <p class="text-base text-center font-light mb-4">
+    <p class="text-base text-center font-medium mb-4" :style="{ color: '#ff8906' }">
       {{ techStack }}
     </p>
-    <div class="flex flex-wrap items-center">
+    <div class="flex flex-wrap items-center md:items-center sm:items-center">
       <social-media :icons="['fab', 'github']" :link="github" />
-      <a :href="url" class="underline ml-3">Demo</a>
+      <a :href="url" class="hover:underline ml-3" target="_blank">Demo</a>
     </div>
   </div>
 </template>
