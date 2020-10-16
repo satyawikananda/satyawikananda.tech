2ac1e751fc4d1dd5ffbd52b6f2ad0b51234d4517
satyawikananda <satyawikananda456@gmail.com>
2020-10-17 00:07:49 +0800

    feat: WIP new nav bottom

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,19 @@
   <div>
     <Navbar />
     <Nuxt />
+    <!-- <Navbottom /> -->
     <Footer />
   </div>
 </template>
 <script>
 import Navbar from '@/components/master/Navbar.vue'
+// import Navbottom from '@/components/master/Navbottom.vue'
 import Footer from '@/components/master/Footer.vue'
 
 export default {
   components: {
     Navbar,
+    // Navbottom,
     Footer
   }
 }
