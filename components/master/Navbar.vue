e7c9c33d2ed143380652e7e02331482a3b370318
satyawikananda <satyawikananda456@gmail.com>
2020-10-18 22:04:10 +0800

    chore: Update navbottom :zap:

--- components/master/Navbar.vue
+++ components/master/Navbar.vue
@@ -2,7 +2,7 @@
   <header class="text-white body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <span class="text-xl ml-3 font-bold" :style="{ color: '#ff8906' }">Satya Wikananda</span>
-      <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+      <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-400 flex flex-wrap items-center text-base justify-center sm:hidden md:hidden lg:block">
         <nuxt-link to="/" class="mr-5 cursor-pointer text-white-semi hover:underline">
           Home
         </nuxt-link>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,19 @@
   <div>
     <Navbar />
     <Nuxt />
-    <!-- <Navbottom /> -->
+    <Navbottom />
     <Footer />
   </div>
 </template>
 <script>
 import Navbar from '@/components/master/Navbar.vue'
-// import Navbottom from '@/components/master/Navbottom.vue'
+import Navbottom from '@/components/master/Navbottom.vue'
 import Footer from '@/components/master/Footer.vue'
 
 export default {
   components: {
     Navbar,
-    // Navbottom,
+    Navbottom,
     Footer
   }
 }
