fd2c96dff416ffa3d1b6cfb743a3beca9f282cd1
satyawikananda <satyawikananda456@gmail.com>
2022-02-18 09:19:59 +0800

    chore: delete unused file

--- src/utils/dark.ts
+++
@@ -1,5 +0,0 @@
-// https://github.com/elucidator-project/elucidator-blog-starter/blob/main/src/utils/dark.ts
-import { useDark, useToggle } from "@vueuse/core"
-
-export const isDark = useDark()
-export const toggleDark = useToggle(isDark)
