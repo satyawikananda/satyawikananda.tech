6cc19548cdd6b36217a74ace39f89796bd241f74
satyawikananda <satyawikananda456@gmail.com>
2022-02-27 16:11:00 +0800

    chore: change favicon and change profilepicture

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,10 +14,19 @@ const { t } = useI18n()
   <main class="m-auto">
     <div class="mx-auto flex mt-10 mb-10 py-8 items-center justify-center flex-col">
       <img
-        class="lg:w-1/4 md:w-2/6 w-3/6 mb-10 w-full h-full object-cover object-center rounded-full"
+        class="lg:w-1/4 md:w-2/6 w-3/6 mb-5 w-full h-full object-cover object-center rounded-full"
         alt="hero"
-        src="https://avatars1.githubusercontent.com/u/33148052?v=4"
+        src="https://yhclghathylvwpslsenl.supabase.in/storage/v1/object/sign/portfolio/142396194_3671723486268036_1816571119795484043_n.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwb3J0Zm9saW8vMTQyMzk2MTk0XzM2NzE3MjM0ODYyNjgwMzZfMTgxNjU3MTExOTc5NTQ4NDA0M19uLmpwZyIsImlhdCI6MTY0NTk0Mzc4NCwiZXhwIjoxOTYxMzAzNzg0fQ.7bZTuFPuZ0M5pzqVBuU6wV83gDxcjkDjCgg1HZMdFKo"
       />
+      <p class="mb-10 text-center text-elucidator-900 dark:text-[#ddd] text-xs">
+        {{ t("home.credit") }}
+        <a
+          class="hover:underline font-semibold"
+          href="https://twitter.com/dittameliaa"
+          target="_blank"
+          >Ditta Amelia</a
+        >
+      </p>
       <div class="text-center w-full w-3/4 lg:w-3/5">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-dark-repulser-500">
           {{ t("home.title") }}
